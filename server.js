9989c01c46ceae63fcb5c900d9ff77c35b9911d7
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2020-02-18 11:58:28 -0600

    Add routes for index.html and notes.html using GET method

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
